7007de4d64e894668c729918d53e8a7c7f14e987
Jair Reina <jair.reina@digitas.com>
2020-12-26 11:05:00 -0600

    adding comments and outputing to the dom

--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
 import { fromEvent, interval } from "rxjs";
+import { switchMapTo } from "rxjs/operators";
 
 const startButton = document.getElementById("start");
+const stopButton = document.getElementById("stop");
+const outputEl = document.getElementById("output");
 
-fromEvent(startButton, "click").subscribe((e) => console.log(e));
+// 1. OUTPUT THE INTERVAL AFTER CLICKING ON A BUTTON
+// createa an observable from an event
+const start$ = fromEvent(startButton, "click");
+// Create an observable from an interval
+const interval$ = interval(1000);
+// Start the interval after the event is triggered using switchMapTo
+const startInterval$ = start$.pipe(switchMapTo(interval$));
 
-interval(1000).subscribe((x) => console.log("interval", x));
+// Output to the console on each interval
+const subscription = startInterval$.subscribe(output);
+
+
+
+
+
+
+function output(str) {
+  outputEl.innerHTML = str.toString();
+}
