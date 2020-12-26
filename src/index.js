364876eb0685d127d70396436f0b57804baaca95
Jair Reina <jair.reina@digitas.com>
2020-12-26 10:30:34 -0600

    adding interval

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-import { fromEvent } from "rxjs";
+import { fromEvent, interval } from "rxjs";
 
 const startButton = document.getElementById("start");
 
 fromEvent(startButton, "click").subscribe((e) => console.log(e));
+
+interval(1000).subscribe((x) => console.log("interval", x));
